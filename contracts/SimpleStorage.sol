a488c8c6bd1b3b5b34e8463158558ce8066980b8
nur <medo0o6665@gmail.com>
2022-04-23 22:54:18 +0800

    creating SimpleStorage contract

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+    // favoriteNumber by default eqauls 0
+    uint256 public favoriteNumber; 
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people; 
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public{
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+    }
+
+    function addPeople(string memory _name, uint256 _favoriteNumber) public{
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+ 
+}
