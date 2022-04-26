9c4629e689d44c441e60020c9214e3235902374c
nur <medo0o6665@gmail.com>
2022-04-27 00:49:08 +0800

    learning how to set up  compile solidity in python

---
+++ deploy.py
@@ -0,0 +1,37 @@
+from msilib.schema import File
+import json
+from solcx import compile_standard,  install_solc
+
+with open("./contracts/SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+install_solc('0.6.0')
+#compile Our Solidity
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = json.loads(
+    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+)["output"]["abi"]
